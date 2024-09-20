4f0df3a0970254b0a74f8a142f10a53410e84658
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-09-20 22:39:29 +0900

    a

---
+++ Editor/CopyScaleAdjuster.cs
@@ -0,0 +1,114 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+using nadena.dev.modular_avatar.core.editor;
+
+using ScaleAdjuster = nadena.dev.modular_avatar.core.ModularAvatarScaleAdjuster;
+using MergeArmature = nadena.dev.modular_avatar.core.ModularAvatarMergeArmature;
+
+namespace Numeira
+{
+    internal static class CopyScaleAdjuster
+    {
+        private const string MenuItemName = "Copy Scale Adjuster";
+        private const string ContextMenuPath = "CONTEXT/ModularAvatarMergeArmature/" + MenuItemName;
+        private const string HierarchyContextMenuPath = "GameObject/Modular Avatar/" + MenuItemName;
+        private const int HierarchyContextMenuPriority = 956;
+
+        internal const string WithSetupOutfitPath = "GameObject/Modular Avatar/Setup Outfit with Copy Scale Adjuster";
+        internal const int WithSetupOutfitPathPriority = -1000;
+
+        #region Context Menu
+
+        [MenuItem(ContextMenuPath, true)]
+        public static bool ValidateMergeArmatureContextMenu(MenuCommand command)
+        {
+            if (command.context is not MergeArmature merge)
+            {
+                merge = (command.context as GameObject)?.GetComponent<MergeArmature>();
+            }
+            
+            return merge.IsValidMergeArmature();
+        }
+
+        [MenuItem(HierarchyContextMenuPath, true, HierarchyContextMenuPriority)]
+        public static bool ValidateHierarchyContextMenu()
+        {
+            return Selection.activeGameObject.GetComponent<MergeArmature>().IsValidMergeArmature();
+        }
+
+        [MenuItem(ContextMenuPath, false)]
+        [MenuItem(HierarchyContextMenuPath, false, HierarchyContextMenuPriority)]
+        public static void OnContextMenu(MenuCommand command)
+        {
+            if (command.context is not MergeArmature merge)
+            {
+                merge = (command.context as GameObject)?.GetComponent<MergeArmature>();
+            }
+
+            Run(merge);
+        }
+
+        private static bool IsValidMergeArmature(this MergeArmature mergeArmature)
+            => mergeArmature != null && mergeArmature.mergeTargetObject != null && mergeArmature.mergeTargetObject.GetComponentInChildren<ScaleAdjuster>(true);
+
+        [MenuItem(WithSetupOutfitPath, false, WithSetupOutfitPathPriority)]
+        public static void SetupOutfitWithCopyScaleAdjuster(MenuCommand command)
+        {
+            if (command.context is not GameObject go)
+                return;
+
+            SetupOutfit.SetupOutfitUI(go);
+            var merge = go.GetComponentInChildren<MergeArmature>();
+
+            Run(merge);
+        }
+
+        #endregion
+
+        public static void Run(MergeArmature merge)
+        {
+            if (merge == null)
+                return;
+
+            var target = merge.mergeTargetObject;
+            var sources = target.GetComponentsInChildren<ScaleAdjuster>(true).WithPath(target, merge.prefix, merge.suffix);
+
+            Undo.SetCurrentGroupName("Copy Scale Adjuster");
+            var idx = Undo.GetCurrentGroup();
+            foreach (var source in sources)
+            {
+                var o = merge.transform.Find(source.Path);
+                if (o != null)
+                {
+                    if (o.TryGetComponent<ScaleAdjuster>(out var c))
+                    {
+                        Undo.RecordObject(c, "Copy Serialized");
+                    }
+                    else
+                    {
+                        c = Undo.AddComponent<ScaleAdjuster>(o.gameObject);
+                    }
+                    EditorUtility.CopySerializedIfDifferent(source.Component, c);
+                }
+            }
+            Undo.CollapseUndoOperations(idx);
+        }
+
+        private static (string Path, T Component)[] WithPath<T>(this T[] components, GameObject target, ReadOnlySpan<char> prefix = default, ReadOnlySpan<char> suffix = default) where T : Component
+        {
+            using var builder = new RelativePathBuilder(stackalloc char[256]);
+            var result = new (string Path, T Component)[components.Length];
+            for(int i  = 0; i < components.Length; i++)
+            {
+                var component = components[i];
+                var path = builder.GetRelativePath(component.gameObject, target, prefix, suffix);
+                builder.Reset();
+                result[i] = (path, component);
+            }
+            return result;
+        }
+    }
+}
+#endif
---
+++ Editor/RelativePathBuilder.cs
@@ -0,0 +1,85 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using System.Buffers;
+
+namespace Numeira
+{
+    internal ref struct RelativePathBuilder
+    {
+        private char[] buffer;
+        private Span<char> span;
+        private int idx;
+
+        public RelativePathBuilder(Span<char> initialBuffer)
+        {
+            if (initialBuffer.IsEmpty)
+            {
+                span = buffer = ArrayPool<char>.Shared.Rent(2048);
+            }
+            else
+            {
+                span = initialBuffer;
+                buffer = null;
+            }
+            idx = span.Length - 1;
+        }
+
+        public string GetRelativePath(GameObject gameObject, GameObject root, ReadOnlySpan<char> prefix = default, ReadOnlySpan<char> suffix = default)
+        {
+            var child = gameObject;
+            if (root == child) return "";
+
+            while (child != root && child != null)
+            {
+                Append(child.name, "/", prefix, suffix);
+                child = child.transform.parent?.gameObject;
+            }
+
+            if (child == null && root != null) return null;
+
+            return ToString();
+        }
+        public override string ToString()
+        {
+            return span[(idx + 1)..].ToString();
+        }
+
+        public void Reset()
+        {
+            idx = span.Length - 1;
+        }
+
+        public void Dispose()
+        {
+            if (buffer != null)
+                ArrayPool<char>.Shared.Return(buffer);
+        }
+
+        private void Append(ReadOnlySpan<char> value, ReadOnlySpan<char> separator, ReadOnlySpan<char> prefix, ReadOnlySpan<char> suffix)
+        {
+            var length = value.Length + separator.Length + prefix.Length + suffix.Length;
+            if (idx - length < 0)
+            {
+                var newBuffer = ArrayPool<char>.Shared.Rent(Math.Max(2048, span.Length + length));
+                var segment = span[idx..];
+                segment.CopyTo(newBuffer.AsSpan()[^segment.Length..]);
+                idx += newBuffer.Length - span.Length;
+                span = buffer = newBuffer;
+            }
+            idx -= length;
+            {
+                var segment = span[idx..];
+                separator.CopyTo(segment);
+                segment = segment[separator.Length..];
+                prefix.CopyTo(segment);
+                segment = segment[prefix.Length..];
+                value.CopyTo(segment);
+                segment = segment[value.Length..];
+                suffix.CopyTo(segment);
+            }
+        }
+
+    }
+}
+#endif
